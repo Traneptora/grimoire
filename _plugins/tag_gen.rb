af6e226e1e0eb0a7ef0a2279691318d0fe09a051
Leo Izen <leo.izen@gmail.com>
2022-01-10 07:37:44 -0500

    tags: add subdomain tags instead

--- _plugins/tag_gen.rb
+++ _plugins/tag_gen.rb
@@ -1,7 +1,7 @@
 # from https://github.com/jfromaniello/joseoncodecom/raw/master/_plugins/tag_gen.rb
 module Jekyll
   class TagIndex < Page
-    def initialize(site, base, dir, tag)
+    def initialize(site, base, dir, tag, subtag)
       @site = site
       @base = base
       @dir = "../"
@@ -10,7 +10,12 @@ module Jekyll
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
       self.data['tag'] = tag
-      self.data['title'] = tag.capitalize
+      self.data['subtag'] = subtag
+      if subtag != nil
+        self.data['title'] = "#{tag} (#{subtag})".capitalize
+      else
+        self.data['title'] = tag.capitalize
+      end
     end
   end
 
@@ -20,12 +25,19 @@ module Jekyll
       if site.layouts.key? 'tag_index'
         dir = site.config['tag_dir'] || 'tags'
         site.tags.keys.each do |tag|
-          write_tag_index(site, File.join(dir, tag), tag)
+          write_tag_index(site, File.join(dir, tag), tag, nil)
+          site.data['classes'].each do |clazz|
+            if clazz['tag'] == tag && clazz['subclasses'] != nil
+              clazz['subclasses'].each do |subclazz|
+                write_tag_index(site, File.join(dir, "#{clazz['tag']}-#{subclazz['subtag']}"), clazz['tag'], subclazz['subtag'])
+              end
+            end
+          end
         end
       end
     end
-    def write_tag_index(site, dir, tag)
-      index = TagIndex.new(site, site.source, dir, tag)
+    def write_tag_index(site, dir, tag, subtag)
+      index = TagIndex.new(site, site.source, dir, tag, subtag)
       index.render(site.layouts, site.site_payload)
       index.write(site.dest)
       site.pages << index

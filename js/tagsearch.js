c91766a05ea187a383eb05a8e263d4dbeab6aeac
Leo Izen <leo.izen@gmail.com>
2023-04-03 23:15:29 -0400

    tagsearch.js: add parenthesized tag grouping to tagSearch

--- js/tagsearch.js
+++ js/tagsearch.js
@@ -1,18 +1,34 @@
+function atomMatches(atom, tagArray) {
+    let flip = false;
+    while (atom.startsWith('!')){
+        atom = atom.substring(1);
+        flip = !flip;
+    }
+    const value = atom === '' || atom === 'true' || atom !== 'false' && tagArray.includes(atom);
+    return flip ^ value;
+}
+
+function matchTagList0(query, tagArray) {
+    const parenRegex = /\([^\(\)]*\)/;
+    let newQuery = query;
+    do {
+        query = newQuery;
+        newQuery = query.replace(parenRegex, (m) => {
+            const v = matchTagList0(m.slice(1, -1), tagArray);
+            return v ? 'true' : 'false';
+        });
+    } while (newQuery !== query);
+
+    return newQuery.split('|').some((part) => part.split('&').every((part2) => atomMatches(part2, tagArray)));
+}
+
 function matchTagList(searchQuery, tagList){
     const tagArray = tagList.split(',').map((tag) => tag.replaceAll(/\s+/g, '').replaceAll(/\..*$/g, '').toLowerCase());
-    return searchQuery.toLowerCase().split('|').some((part) => part.split('&').every((part2) => {
-        let atom = part2.replaceAll(/\s+/g, '');
-        let flip = false;
-        while (atom.startsWith('!')){
-            atom = atom.substring(1);
-            flip = !flip;
-        }
-        return flip ^ tagArray.includes(atom);
-    }));
+    return matchTagList0(searchQuery.replaceAll(/\s+/g, ''), tagArray);
 }
 
 function runTagSearch(bar, dataEntry, classPrefix){
-    const searchQuery = document.getElementById(bar).value;
+    const searchQuery = document.getElementById(bar).value.toLowerCase();
     Array.from(document.querySelectorAll("li.post-link-container")).filter((elem) => {
         return !!elem.dataset[dataEntry];
     }).forEach((elem) => {
